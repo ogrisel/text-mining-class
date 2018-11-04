669ffd8d7de05bd48008b8730dc56d72ae492ea5
Olivier Grisel <olivier.grisel@ensta.org>
2018-11-04 11:50:12 +0100

    Started exercise module on text manipulation with encoding

---
+++ solutions/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+setup(
+    name='text-mining-class',
+    version='1.0.0.dev0',
+    description="Introduction to text processing and mining",
+    author='Olivier Grisel',
+    author_email='olivier.grisel@ensta.org',
+    zip_safe=False,
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering',
+    ],
+    platforms='any',
+    install_requires=[],  # TODO
+    tests_require=['pytest'],
+)
