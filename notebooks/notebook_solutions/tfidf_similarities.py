a215cfb6b806caac9a843409fcf793e64f7cc6dd
Olivier Grisel <olivier.grisel@ensta.org>
2018-11-28 14:09:02 +0100

    Small improvements to TF-IDF notebook

--- notebooks/notebook_solutions/tfidf_similarities.py
+++ notebooks/notebook_solutions/tfidf_similarities.py
@@ -1,21 +1,27 @@
 %matplotlib inline
 import pandas as pd
 import matplotlib.pyplot as plt
+from pathlib import Path
 
 
 sorted_similarities = pd.DataFrame({
-    "doc_id": np.arange(len(similarities)) + 1,
-    "similarity": similarities,
+    "filepath": text_filepaths[1:],
     "category": categories[1:],
+    "similarity": similarities,
 }).sort_values("similarity", ascending=False)
 
+# Most related document
 print(sorted_similarities.head(15))
+
+# Distribution of all cosine similarities w.r.t. the first
+# document.
 plt.plot(sorted_similarities["similarity"].values);
 
-closest_doc_id = int(sorted_similarities['doc_id'].iloc[0])
+# Most similar document:
+closest_doc_path = Path(sorted_similarities['filepath'].iloc[0])
 
 print("Query document:")
 print(text_filepaths[0].read_text(encoding="utf-8"), end="\n\n")
 
 print("First result document document:")
-print(text_filepaths[closest_doc_id].read_text(encoding="utf-8"), end="\n\n")
+print(closest_doc_path.read_text(encoding="utf-8"), end="\n\n")
